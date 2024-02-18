239f48fe41237fe185834cd1a88d447f4d484bd3
Aryaman555 <115450762+Aryaman555@users.noreply.github.com>
2024-02-18 17:12:21 +0530

    Add files via upload

---
+++ Module1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ParallelVectorSum
+{
+    class Program
+    {
+        static void RandomVector(int[] vector, int size)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                vector[i] = rand.Next(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int size = 100000000;
+            int[] v1 = new int[size];
+            int[] v2 = new int[size];
+            int[] v3 = new int[size];
+            Random rand = new Random();
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            RandomVector(v1, size);
+            RandomVector(v2, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                v3[i] = v1[i] + v2[i];
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("Time taken by function: " + stopwatch.ElapsedMilliseconds + " milliseconds");
+        }
+    }
+}
